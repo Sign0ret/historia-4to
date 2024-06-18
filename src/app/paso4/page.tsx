f3e42bb2519f81f3bfc5da547a2aecffd9de8e1f
Sign0ret <adolfo.h.signoret@gmail.com>
2024-06-18 16:09:44 -0600

    texto

--- src/app/paso4/page.tsx
+++ src/app/paso4/page.tsx
@@ -12,59 +12,21 @@ export default function Paso4() {
 
 const dummyContent = [
     {
-      title: "No fue fácil",
+      title: "Adaptándome al Cambio",
       description: (
         <>
           <p>
-            Hubo un momento en mi semestre, durante mi materia de Métodos
-            Computacionales, en la que sentí que no sabía nada. Fue cuando
-            el profesor nos otorgó la gran oportunidad de hacer un sistema
-            de autómatas de pila para resolver el ejercicio de fibonnaci,
-            con la condición de que debíamos de utilizar backtracking.
-          </p>
-          <p>
-            Hice muchos intentos para hacerlo, incluso me junté con otros 
-            miembros del salón por las tardes a lograr desarrollarlo.
+            Cuando logré aprender autómatas, logré dominar el reto de web,
+            y ya me sentía agusto tocando violín, me di cuenta de que aunque
+            quizá no estaba logrando todo lo que hubiera querido lograr en
+            el ámbito laboral, estaba logrando mucho en el ámbito escolar y 
+            personal
           </p>
         </>
       ),
-      badge: "Backtracking",
+      badge: "Aprendiendo",
       image:
         "",
-    },
-    {
-      title: "Una date muy romántica",
-      description: (
-        <>
-          <p>
-            Como no logré resolverlo en la semana, me llevé el problema de 
-            tarea, pero había otro asunto, había invitado a mi novia a salir.
-          </p>
-          <p>
-            Como ella es tan comprensiva, salimos a cenar pero me puse a hacer
-            el ejercicio en un cuaderno en la mesa del restaurante. Claro que
-            no hubiera importado si no lo hubiera hecho, pues aquí no logré
-            resolverlo tampoco.
-          </p>
-        </>
-      ),
-      badge: "Modo mandilón",
-      image: ""
-    },
-    {
-      title: "Si se pudo",
-      description: (
-        <>
-          <p>
-            Al final, un compañero pudo obtener un autómata que funcionara,
-            entonces ya solo era cuestión de codearlo, y hacer que funcionara.
-            Claro que se dice fácil, pero después de 3 días de pruebas y errores,
-            logré tener el autómata funcionando en casi todos los casos.
-          </p>
-        </>
-      ),
-      badge: "Victory",
-      image: ""
     },
   ];
   
--- src/app/paso5/page.tsx
+++ src/app/paso5/page.tsx
@@ -1,7 +1,37 @@
+import { TracingContent } from '@/components/tracing-content'
 import React from 'react'
+
+
 
 export default function Paso5() {
   return (
-    <div>Paso5</div>
+    <TracingContent dummyContent={dummyContent} next="smart"/>
   )
 }
+
+
+const dummyContent = [
+    {
+      title: "Nuevo Inicio",
+      description: (
+        <>
+          <p>
+            Terminando el semestre, el proyecto de Xavio AI me
+            hizo sentir tan alegre con lo que habíamos construido
+            en equipo, que ni siquiera me importaba ya la calificación
+            o los requisitos, le hechaba ganas solo por el gusto de que
+            estuviera cada vez mejor.
+          </p>
+          <p>
+            Aprendí que no siempre se debe de buscar la calificación o el
+            dinero, sino también la satisfacción al hacer lo que a uno le 
+            gusta y sabe hacer.
+          </p>
+        </>
+      ),
+      badge: "Cierre",
+      image:
+        "",
+    }
+  ];
+  
